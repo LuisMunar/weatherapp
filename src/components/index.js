176509922b499bcec910584a79357faf1e321e75
LuisMunar <luis.emunarr@gmail.com>
2018-12-24 23:07:10 -0500

    ok video 39 section 6

--- src/components/index.js
+++ src/components/index.js
@@ -4,23 +4,49 @@ import Location from './Location';
 import WeatherData from './WeatherData/';
 
 import './style.scss';
+
 import {
-    SUNNY
+    SUNNY,
+    FOG
 } from '../constants/weathers';
 
-const data = {
+const dataUno = {
     temperatureProps: 5,
     weatherStateProps: SUNNY,
     humidityProps: 6,
     windProps: 7
 }
+const dataDos = {
+    temperatureProps: 15,
+    weatherStateProps: FOG,
+    humidityProps: 8,
+    windProps: 11
+}
 
 class WeatherLocation extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cityState: 'Armenia',
+            dataState: dataUno
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            cityState: 'Bogota',
+            dataState: dataDos
+        });
+    }
+
     render() {
+        const { cityState, dataState } = this.state;
+
         return (
             <div className="WeatherLocation">
-                <Location city={'Armenia'} />
-                <WeatherData dataProps={data} />
+                <Location city={cityState} />
+                <WeatherData dataProps={dataState} />
+                <button onClick={this.handleClick}>Actualizar</button>
             </div>
         )
     }
