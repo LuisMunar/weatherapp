d82fc26395225e96db0bbb259a64d8b6bb8de70b
LuisMunar <luis.emunarr@gmail.com>
2019-01-18 22:11:07 -0500

    section 10 video 76

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 import './App.scss';
 import LocationList from './components/LocationList';
@@ -18,12 +23,35 @@ class App extends Component {
     ]
 
     return (
-      <div className="App">
-        <LocationList
-          cities={cities}
-          onSelectedLocation={this.handleSelectedLocation}
-        />
-      </div>
+      <Grid>
+
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList
+              cities={cities}
+              onSelectedLocation={this.handleSelectedLocation}
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper elevation={4} className='details-super'>
+              <div className='details'>
+                dsdsd
+            </div>
+            </Paper>
+          </Col>
+        </Row>
+
+      </Grid>
     );
   }
 }
