b2bdbfcf684cfd1d368977637a50d91112f3ec8e
LuisMunar <luis.emunarr@gmail.com>
2019-02-28 22:25:24 -0500

    section 13 video 121

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
+import PropTypes from 'prop-types';
 
 // Impotacion de componentes.
 import LocationList from './components/LocationList';
 import ForecastExtended from './components/ForecastExtended';
 
-// Importancion del store redux.
-import { store } from './sotore';
+// Importancion de connect para conectarnos al store redux.
+import { connect } from 'react-redux';
 
 // Importacion de acciones para el store redux.
 import { setCity } from './actions';
@@ -34,7 +35,7 @@ class App extends Component {
     console.log(`handleSelectedLocation ${city}`);
 
     // Con esta liena estamo enviando la action importada desde el archivo que contiene las actions al store. Lo podemos evidenciar en la pestaña de la herramienta devtool redux chroome.
-    store.dispatch(setCity(city));
+    this.props.dispatchSetCity(city);
   }
 
   render() {
@@ -82,4 +83,21 @@ class App extends Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  dispatchSetCity : PropTypes.func.isRequired
+};
+
+/* Esta funcion es el segundo parametro que recibe la funcion "connect", debe recibir como parametro el metodo "dispatch" de redux. Esta funcion debe retornar
+un objeto, cuya clave del objeto va a ser un props valido para el componente de clase. Para asignarle un valor a esta clave props, esta misma debe valer una funcion
+con su respectivo parametro (solo si aplica) y esta va a retornar el metodo dispatch con parametro la action que importamos previamente, en este caso
+"setCity(value)"
+*/
+const mapDispatchToPorps = (dispatch) => (
+  {
+    dispatchSetCity : value => dispatch(setCity(value))
+  }
+);
+
+/* La siguiente linea exporta por defecto el componente de clase con sus respectivos actions, states y props al store. La funcion "connect" y el resultado
+de esta misma es otra funcion la cual pide un parametro, cuyo parametro debe ser el nombre del componente de clase, en este caso "App" */
+export default connect(null, mapDispatchToPorps)(App);
